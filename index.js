c89dbe8794bfbe9e48b7bdd7caffbb16034e0765
maheshkp100 <mahesh.panchmal@gmail.com>
2021-01-10 15:04:59 +0000

    first build

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+ 
+const host = '0.0.0.0';
+const port = 8080;
+ 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World 12345!');
+});
+ 
+server.listen(port, host, () => {
+   console.log('Web server running at http://%s:%s',host,port );
+});
